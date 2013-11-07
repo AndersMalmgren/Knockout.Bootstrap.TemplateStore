fd85215c08381f26c0d44f57a24c674b65ab5408
AndersMalmgren <anders.malmgren@agero.se>
2013-11-07 16:02:27 +0100

    Added SystemWeb project

---
+++ Knockout.Bootstrap.TemplateStore.SystemWeb/Owin/AppBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Hosting;
+using Owin;
+
+namespace Knockout.Bootstrap.TemplateStore.SystemWeb.Owin
+{
+    public static class AppBuilderExtensions
+    {
+        public static void InitTemplateStore(this IAppBuilder app, string templateVirtualDirtectory, string fileMask)
+        {
+            Bootstrap.TemplateStore.Owin.AppBuilderExtensions.InitTemplateStore(app, HostingEnvironment.MapPath(templateVirtualDirtectory), fileMask);
+        }
+
+        public static void InitTemplateStore(this IAppBuilder app)
+        {
+            app.InitTemplateStore("~/Views", "*.htm*");
+        }
+    }
+}
